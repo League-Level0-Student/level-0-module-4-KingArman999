52607047962545b2faaa50d7f6050800867161aa
KingArman999 <44267696+KingArman999@users.noreply.github.com>
2019-02-12 19:51:28 -0800

    For Loops

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,14 +15,19 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String digits = JOptionPane.showInputDialog(null, "How many nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+int boi = Integer.parseInt(digits);
 		// Ask the user how many dimes they have, and convert their answer
-
+String poi = JOptionPane.showInputDialog(null, "How many dimes do you have?");
+int goi = Integer.parseInt(poi);
 		// Ask the user how many quarters they have, and convert their answer
-
+String koi = JOptionPane.showInputDialog(null, "How many quarters do you have?");
+int loi = Integer.parseInt(koi);
 		// Calculate how much money the user has and save it in a double variable 
+double soi = 0.0;
+soi += boi*0.5;
+
 
 		// Tell the user how much money they have
 
